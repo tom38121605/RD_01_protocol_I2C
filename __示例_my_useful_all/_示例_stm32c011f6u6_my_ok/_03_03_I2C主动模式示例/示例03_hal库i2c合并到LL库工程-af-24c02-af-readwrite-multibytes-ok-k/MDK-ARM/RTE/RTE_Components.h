179f52a7a72c96014142634ab7847d1c73d7adf2
tom38121605 <13538121605@139.com>
2024-07-26 12:17:55 +0800

    lit

---
+++ __示例_my_useful_all/_示例_stm32c011f6u6_my_ok/_03_03_I2C主动模式示例/示例03_hal库i2c合并到LL库工程-af-24c02-af-readwrite-multibytes-ok-k/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'soma_patient_reference_12615' 
+ * Target:  'soma_patient_reference_12615' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
